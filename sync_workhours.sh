5dddade41d75986b0badb79fc9066d965a1ff6d7
julianbarg <barg.julian@gmail.com>
2019-10-22 00:36:47 -0400

    Improved bash script for auto-commit.

--- sync_workhours.sh
+++ sync_workhours.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+printf "\n\n\n"
+date +%d-%m-%y/%H:%M:%S
+echo "--------------------------"
 . ~/.keychain/`/bin/hostname`-sh
 cd "$(dirname "$0")"
 git commit timestamps.csv -m auto-commit
